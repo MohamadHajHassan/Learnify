48641361d359926d9397d54199135ce03ffa4dbe
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2024-07-17 17:11:47 +0300

    Add User class

---
+++ Learnify-backend/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Learnify_backend.Entities
+{
+    public class User
+    {
+        [BsonId]
+        [BsonElement("_id"), BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("username"), BsonRepresentation(BsonType.String)]
+        public string? Username { get; set; }
+    }
+}
