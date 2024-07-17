f5586f55d7b7e71ddbfac994fca46915b42b47ec
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2024-07-17 17:10:43 +0300

    Add MongoDbService for DB connectivity

--- Learnify-backend/Program.cs
+++ Learnify-backend/Program.cs
@@ -1,6 +1,10 @@
+using Learnify_backend.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddSingleton<MongoDbService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
