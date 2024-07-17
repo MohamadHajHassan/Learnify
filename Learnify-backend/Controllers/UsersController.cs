b6ff01d08056fb66aec740929402cbe3e3662f25
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2024-07-17 17:15:06 +0300

    Add UsersController

---
+++ Learnify-backend/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using Learnify_backend.Data;
+using Learnify_backend.Entities;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Learnify_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IMongoCollection<User> _users;
+
+        public UsersController(MongoDbService mongoDbService)
+        {
+            _users = mongoDbService.Database.GetCollection<User>("users");
+        }
+
+        // GET: api/<UsersController>
+        [HttpGet]
+        public async Task<IEnumerable<User>> Get()
+        {
+            return await _users.Find(FilterDefinition<User>.Empty).ToListAsync();
+        }
+    }
+}
