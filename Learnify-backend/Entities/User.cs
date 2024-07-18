6f458402fdc803243c990c4b8db2c0786ca30f66
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2024-07-18 12:18:26 +0300

    Add new properties to User class

--- Learnify-backend/Entities/User.cs
+++ Learnify-backend/Entities/User.cs
@@ -11,5 +11,29 @@ namespace Learnify_backend.Entities
 
         [BsonElement("username"), BsonRepresentation(BsonType.String)]
         public string? Username { get; set; }
+
+        [BsonRequired]
+        [BsonElement("email"), BsonRepresentation(BsonType.String)]
+        public string? Email { get; set; }
+
+        [BsonRequired]
+        [BsonElement("password"), BsonRepresentation(BsonType.String)]
+        public string? Password { get; set; } // Should be hashed before storing
+
+        [BsonElement("firstName"), BsonRepresentation(BsonType.String)]
+        public string? FirstName { get; set; }
+
+        [BsonElement("lastName"), BsonRepresentation(BsonType.String)]
+        public string? LastName { get; set; }
+
+        [BsonElement("profilePicture"), BsonRepresentation(BsonType.String)]
+        public string? ProfilePicture { get; set; }
+
+        [BsonElement("role"), BsonRepresentation(BsonType.String)]
+        public string? Role { get; set; }  // "student", "instructor", "admin"
+
+        [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
+        [BsonElement("createdOn"), BsonRepresentation(BsonType.DateTime)]
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
     }
 }
