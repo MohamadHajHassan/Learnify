96cd14a59d31e41a10d1b11e1f2928c9b907cecb
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2024-07-19 23:24:43 +0300

    Add Module entity

---
+++ Learnify-backend/Entities/Module.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Learnify_backend.Entities
+{
+    public class Module
+    {
+        [BsonId]
+        [BsonElement("_id"), BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonRequired]
+        [BsonElement("ordre"), BsonRepresentation(BsonType.Int32)]
+        public int Ordre { get; set; }
+
+        [BsonRequired]
+        [BsonElement("courseId"), BsonRepresentation(BsonType.ObjectId)]
+        public string? CourseId { get; set; }
+
+        [BsonElement("lessonsId"), BsonRepresentation(BsonType.String)]
+        public IEnumerable<string> LessonsId { get; set; } = new List<string>();
+
+        [BsonElement("quizId"), BsonRepresentation(BsonType.String)]
+        public string? QuizId { get; set; }
+    }
+}
