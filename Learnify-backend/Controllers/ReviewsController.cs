b94aa6974fe5ce2c9b38ad183f0a12997768cf1f
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2024-07-19 19:22:51 +0300

    Add ReviewsController with CRUD operations

---
+++ Learnify-backend/Controllers/ReviewsController.cs
@@ -0,0 +1,64 @@
+﻿using Learnify_backend.Data;
+using Learnify_backend.Entities;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Learnify_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewsController : ControllerBase
+    {
+        private readonly IMongoCollection<Review> _reviews;
+
+        public ReviewsController(MongoDbService mongoDbService)
+        {
+            _reviews = mongoDbService.Database.GetCollection<Review>("reviews");
+        }
+
+        // GET: api/<ReviewsController>
+        [HttpGet]
+        public async Task<IEnumerable<Review>> GetAllreviews()
+        {
+            return await _reviews.Find(FilterDefinition<Review>.Empty).ToListAsync();
+        }
+
+        // GET api/<ReviewsController>/5
+        [HttpGet("{id}")]
+        public ActionResult<Review> GetreviewById(string id)
+        {
+            var filter = Builders<Review>.Filter.Eq(x => x.Id, id);
+            var review = _reviews.Find(filter).FirstOrDefault();
+            return review is not null ? Ok(review) : NotFound();
+        }
+
+        // POST api/<ReviewsController>
+        [HttpPost]
+        public async Task<ActionResult> Createreview(Review review)
+        {
+            await _reviews.InsertOneAsync(review);
+            return CreatedAtAction(nameof(GetreviewById), new { id = review.Id }, review);
+        }
+
+        // PUT api/<ReviewsController>/5
+        [HttpPut]
+        public async Task<ActionResult> Updatereview(Review review)
+        {
+            var filter = Builders<Review>.Filter.Eq(x => x.Id, review.Id);
+            await _reviews.ReplaceOneAsync(filter, review);
+            return Ok();
+        }
+
+        // DELETE api/<ReviewsController>/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Deletereview(string id)
+        {
+            var filter = Builders<Review>.Filter.Eq(x => x.Id, id);
+            await _reviews.DeleteOneAsync(filter);
+            return Ok();
+        }
+    }
+}
+
