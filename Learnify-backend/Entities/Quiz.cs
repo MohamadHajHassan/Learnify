97fd87df54a17e72d1012e82e890f663087db94f
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2024-07-19 23:21:56 +0300

    Add Quiz entity

---
+++ Learnify-backend/Entities/Quiz.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Learnify_backend.Entities
+{
+    public class Quiz
+    {
+        [BsonId]
+        [BsonElement("_id"), BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonRequired]
+        [BsonElement("moduleId"), BsonRepresentation(BsonType.ObjectId)]
+        public string? ModuleId { get; set; }
+
+        [BsonElement("questionsId"), BsonRepresentation(BsonType.String)]
+        public IEnumerable<string> QuestionsId { get; set; } = new List<string>();
+    }
+}
