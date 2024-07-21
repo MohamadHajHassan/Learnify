35b18d01263662e4de078fd80c8648409484937d
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2024-07-21 10:19:38 +0300

    Add Grade entity

---
+++ Learnify-backend/Entities/Grade.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Learnify_backend.Entities
+{
+    public class Grade
+    {
+        [BsonId]
+        [BsonElement("_id"), BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonRequired]
+        [BsonElement("userId"), BsonRepresentation(BsonType.ObjectId)]
+        public string? UserId { get; set; }
+
+        [BsonRequired]
+        [BsonElement("quizId"), BsonRepresentation(BsonType.ObjectId)]
+        public string? QuizId { get; set; }
+
+        [BsonElement("score"), BsonRepresentation(BsonType.Int32)]
+        public int Score { get; set; }
+
+        [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
+        [BsonElement("createdOn"), BsonRepresentation(BsonType.DateTime)]
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+    }
+}
