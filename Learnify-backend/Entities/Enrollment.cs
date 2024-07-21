4c6623e32357329e3e80a6b72da685396f67802d
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2024-07-21 13:46:01 +0300

    Add Enrollment entity

---
+++ Learnify-backend/Entities/Enrollment.cs
@@ -0,0 +1,33 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Learnify_backend.Entities
+{
+    public class Enrollment
+    {
+        [BsonId]
+        [BsonElement("_id"), BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonRequired]
+        [BsonElement("userId"), BsonRepresentation(BsonType.ObjectId)]
+        public string? UserId { get; set; }
+
+        [BsonRequired]
+        [BsonElement("courseId"), BsonRepresentation(BsonType.ObjectId)]
+        public string? CourseId { get; set; }
+
+        [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
+        [BsonElement("enrolledOn"), BsonRepresentation(BsonType.DateTime)]
+        public DateTime EnrolledOn { get; set; } = DateTime.UtcNow;
+
+        [BsonElement("progress"), BsonRepresentation(BsonType.Double)]
+        public double Progress { get; set; } = 0;
+
+        [BsonElement("completedModulesId"), BsonRepresentation(BsonType.String)]
+        public IEnumerable<string> CompletedModulesId { get; set; } = new List<string>();
+
+        [BsonElement("gradesId"), BsonRepresentation(BsonType.String)]
+        public IEnumerable<string> GradesId { get; set; } = new List<string>();
+    }
+}
