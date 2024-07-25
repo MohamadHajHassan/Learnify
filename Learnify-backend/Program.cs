e2846dd7fb498ffa92a0bcde24132c34120de21d
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2024-07-25 23:07:48 +0300

    Add JWT authentication

--- Learnify-backend/Program.cs
+++ Learnify-backend/Program.cs
@@ -1,4 +1,7 @@
 using Learnify_backend.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,11 +9,31 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton<MongoDbService>();
 
+builder.Services.AddScoped<JWTTokenGenerator>();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddAuthentication(cfg =>
+{
+    cfg.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    cfg.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(builder.Configuration["Token:SecretKey"])),
+        ValidIssuer = builder.Configuration["Token:Issuer"],
+        ValidateIssuer = true,
+        ValidateLifetime = true,
+        ValidateAudience = false,
+        ValidateActor = false,
+        ValidateTokenReplay = false,
+    };
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,6 +44,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
