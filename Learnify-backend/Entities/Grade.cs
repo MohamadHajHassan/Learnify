3a51c942ec6ea3997e5c10991a6150233e3a6d44
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2024-07-31 17:11:40 +0300

    Refactor Grade entity

--- Learnify-backend/Entities/Grade.cs
+++ Learnify-backend/Entities/Grade.cs
@@ -10,18 +10,36 @@ namespace Learnify_backend.Entities
         public string? Id { get; set; }
 
         [BsonRequired]
-        [BsonElement("userId"), BsonRepresentation(BsonType.ObjectId)]
-        public string? UserId { get; set; }
+        [BsonElement("enrollmentId"), BsonRepresentation(BsonType.ObjectId)]
+        public string? EnrollmentId { get; set; }
 
         [BsonRequired]
         [BsonElement("quizId"), BsonRepresentation(BsonType.ObjectId)]
         public string? QuizId { get; set; }
 
-        [BsonElement("score"), BsonRepresentation(BsonType.Int32)]
-        public int Score { get; set; }
+        [BsonElement("userAnswers"), BsonRepresentation(BsonType.Document)]
+        public Dictionary<string, UserAnswerResult> UserAnswers { get; set; } = new Dictionary<string, UserAnswerResult>();
+
+        [BsonElement("score"), BsonRepresentation(BsonType.Double)]
+        public double Score { get; set; }
+
+        [BsonElement("highestScore"), BsonRepresentation(BsonType.Double)]
+        public double HighestScore { get; set; } = 0.0;
+
+        [BsonElement("isPassed"), BsonRepresentation(BsonType.Boolean)]
+        public bool IsPassed { get; set; }
 
         [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
         [BsonElement("createdOn"), BsonRepresentation(BsonType.DateTime)]
         public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+    }
+
+    public class UserAnswerResult
+    {
+        [BsonElement("userAnswer"), BsonRepresentation(BsonType.String)]
+        public required string UserAnswer { get; set; }
+
+        [BsonElement("isCorrect"), BsonRepresentation(BsonType.Boolean)]
+        public bool IsCorrect { get; set; }
     }
 }
